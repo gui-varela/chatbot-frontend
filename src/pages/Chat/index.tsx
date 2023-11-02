75f768c37c3fbc5289c35c4ca00cb3051e42c283
gvarela <guilhermevareladossantos@gmail.com>
2023-11-02 02:11:31 -0300

    Builds the chat structure and styles

--- src/pages/Chat/index.tsx
+++ src/pages/Chat/index.tsx
@@ -1,28 +1,110 @@
 import { useState } from 'react';
-import { Container } from './styles';
+import { Container, TypingAnimation } from './styles';
+
+import botImage from '../../assets/boticon.jpg'
+
+interface MessageGroup {
+  messages: string[];
+  sentByUser?: boolean;
+}
 
 export function Chat() {
+  const questions = [
+    {
+      messages: ['Olá! Me chamo bot, vamos começar nosso atendimento?', 'Qual seu nome?'],
+      sentByUser: false,
+    },
+    {
+      messages: ['Ok blz', 'Qual seu peso?', 'Nao precisa ter vergonha hein rsrs'],
+      sentByUser: false,
+    },
+    {
+      messages: ['Suave', 'Tem sentido fome?', 'Dá uma nota de 0 a 10'],
+      sentByUser: false,
+    }
+  ]
+
+  const initialMessages: MessageGroup[] = [
+    {
+      messages: ['Olá! Me chamo bot, vamos começar nosso atendimento?', 'Qual seu nome?'],
+      sentByUser: false,
+    },
+    {
+      messages: ['Oii', 'Me chamo Guiii', 'Vamboraaaa'],
+      sentByUser: true,
+    },
+    {
+      messages: ['Olá! Me chamo bot, vamos começar nosso atendimento?', 'Qual seu nome?'],
+      sentByUser: false,
+    },
+  ]
+
+  const emptyGroup = { messages: [] }
+
   const [newMessage, setNewMessage] = useState<string>('')
-  const [userMessages, setUserMessages] = useState<string[]>(['dsfdsfdsf', 'asdsadsadsad'])
+  const [messageGroup, setMessageGroup] = useState<MessageGroup>(emptyGroup)
+  const [messages, setMessages] = useState<MessageGroup[]>(initialMessages)
+  const [currentQuestion, setCurrentQuestion] = useState(0)
 
   const sendMessage = () => {
-    setUserMessages([...userMessages, newMessage])
-    console.log(userMessages)
+    const userMessage = newMessage
+    const userMessageGroup = {
+      messages: [userMessage],
+      sentByUser: true
+    }
+    const botMessageGroup = questions[currentQuestion]
+
+    setMessageGroup(userMessageGroup)
+    setMessages([...messages, userMessageGroup, botMessageGroup])
+    setNewMessage('')
+    setMessageGroup(emptyGroup)
+    setCurrentQuestion(currentQuestion + 1)
+  }
+
+  function typingAnimation() {
+    return (
+      <TypingAnimation>
+        <span></span>
+        <span></span>
+        <span></span>
+      </TypingAnimation>
+    )
   }
 
   return (
     <Container>
       <div className="messagesContainer">
-        {userMessages}
+        {messages.map((messageGroup) => {
+          console.log(messageGroup.sentByUser)
+          return (
+            messageGroup.sentByUser
+              ?
+              <div className={'message userMessage'}>
+                {messageGroup.messages.map(message => <p>{message}</p>)}
+              </div>
+              :
+              <div className={'messageGroup'}>
+                <img src={botImage} alt="" />
+                <div className='message botMessage'>
+                  {messageGroup.messages.map((message) => {
+                    return <p>{message}</p>
+                  })}
+                </div>
+              </div>
+
+          )
+        })}
       </div>
       <form onSubmit={(e) => e.preventDefault()} className='inputForm' action="">
         <input
           type="text"
           name="inputMessage"
+          placeholder='Digite uma mensagem...'
           value={newMessage}
           onChange={(e) => setNewMessage(e.target.value)}
+          autoComplete='off'
         />
-        <button onClick={sendMessage} type="submit">Env</button>
+        <button onClick={sendMessage} type="submit">Enviar</button>
       </form>
     </Container>
   );
--- src/pages/Chat/styles.ts
+++ src/pages/Chat/styles.ts
@@ -10,11 +10,71 @@ export const Container = styled.div`
   margin: 5vh 0;
   border-radius: 5px;
   overflow: hidden;
-  background-color: #FFDFDF;
+  background-color: #ececff;
   word-wrap: break-word;
+  box-shadow: 0 15px 25px #655DBB25;
 
   .messagesContainer {
     flex-grow: 10;
+    padding: 2rem 1rem 0;
+    box-sizing: border-box;
+    overflow-y: scroll;
+
+    .message {
+      display: flex;
+      flex-direction: column;
+      max-width: 100%;
+
+      p {
+        text-align: left;
+        box-sizing: border-box;
+        margin: 1px;
+        padding: .5rem;
+        max-width: 80%;
+        min-width: 15%;
+        width: fit-content;
+      }
+    }
+    
+    .userMessage {
+      align-items: flex-end;
+      margin-bottom: 1rem;
+
+      p {
+        background-color: #5449c9;
+        border-radius: 15px 15px 15px 15px;
+      }
+    }
+
+    .messageGroup {
+      display: flex;
+      align-items: flex-start;
+      flex-direction: row;
+      margin-bottom: 1rem;
+
+      img {
+        height: 2rem;
+        width: 2rem;
+        border-radius: 50%;
+        margin-right: .25rem;
+      }
+
+      .botMessage {
+        p {
+          background-color: #fff;
+          border-radius: 5px 15px 15px 5px;
+          color: #595763;
+        }
+
+        :first-child {
+          border-radius: 2px 15px 15px 5px;
+        }
+
+        :last-child  {
+          border-radius: 5px 15px 15px 15px;
+        }
+      }
+    }
   }
 
   .inputForm {
@@ -22,10 +82,11 @@ export const Container = styled.div`
     flex-direction: row;
     box-sizing: border-box;
     height: 70px;
-    background-color: #FFDFDF;
-    padding: 1rem;
+    background-color: #ececff;
+    padding: .75rem;
     bottom: 0;
     width: 100%;
+    border-top: 1px solid #dedeff;
 
     input[type='text'] {
       width: 100%;
@@ -34,11 +95,33 @@ export const Container = styled.div`
       border: none;
       padding-left: .75rem;
       background-color: #fff;
+      color: #595763;
+      box-shadow: 0 5px 10px #655DBB10;
+    } 
+
+    input[type='text']:focus {
+      border: none;
+    } 
+
+    input::placeholder {
+      color: #bab6ca;
+      font-style: italic;
     }
 
     button {
-      background-color: #F875AA;
+      background-color: #655DBB;
       border-radius: 70px;
+      font-weight: bolder;
+      padding: 0 .75rem;
+      transition: .4s;
+    }
+
+    button:hover {
+      background-color: #4d45a5;
     }
   }
+`;
+
+export const TypingAnimation = styled.div`
+
 `;
